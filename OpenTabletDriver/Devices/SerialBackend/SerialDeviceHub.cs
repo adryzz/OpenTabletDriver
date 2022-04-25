3e8ed45790712aed00e7da562db298daab2cf598
Adryzz <adryzz@vignetteapp.org>
2022-04-25 03:14:59 +0200

    add initial serial tablet support

---
+++ OpenTabletDriver/Devices/SerialBackend/SerialDeviceHub.cs
@@ -0,0 +1,33 @@
+using System.IO.Ports;
+using OpenTabletDriver.Plugin.Attributes;
+using OpenTabletDriver.Plugin.Devices;
+
+namespace OpenTabletDriver.Devices.SerialBackend
+{
+    [DeviceHub]
+    public class SerialDevoceHub : ILegacyDeviceHub
+    {
+        public SerialDevoceHub()
+        {
+            
+        }
+
+        public bool CanEnumeratePorts => true;
+
+        public string[] EnumeratePorts() => SerialPort.GetPortNames();
+
+        public bool TryGetDevice(string path, out IDeviceEndpoint endpoint)
+        {
+            try
+            {
+                endpoint = new SerialInterface(path);
+                return true;
+            }
+            catch
+            {
+                endpoint = null;
+                return false;
+            }
+        }
+    }
+}
