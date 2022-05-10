b18b07da269f938fee7b57f1350ad0298a4b5745
Adryzz <adryzz@vignetteapp.org>
2022-05-10 20:15:48 +0200

    implement UX

--- OpenTabletDriver.UX/Windows/Tablet/LegacyTabletConnectionWindow.cs
+++ OpenTabletDriver.UX/Windows/Tablet/LegacyTabletConnectionWindow.cs
@@ -23,9 +23,56 @@ namespace OpenTabletDriver.UX.Windows.Tablet
             : base(Application.Instance.MainForm)
         {
             Title = "Connect legacy tablet...";
-
+            Icon = App.Logo.WithSize(App.Logo.Size);
+            ClientSize = new Size(300, 250);
             
+
+            var connectButton = new Button
+            {
+                Text = "Connect",
+            };
+
+
+            /*connectButton.Click += async (_, _) => await Connect(devicePathText.Text,
+                (s) => deviceStringText.Text = s,
+                (e) => MessageBox.Show($"Error: {e.Message}", MessageBoxType.Error),
+                () => MessageBox.Show(OperationTimedOut)
+            );*/
+
+            devicePathText = new ComboBox();
+
+            tablet = new DropDown<string>();
+
+            devicePathGroup = new Group("Device path", devicePathText, Orientation.Vertical, false);
+
+            tabletGroup = new Group("Tablet", tablet, Orientation.Vertical, false);
+            
+            reconnectBox = new CheckBox
+            {
+                Text = "Remember tablet"
+            };
+
+            Content = new StackLayout
+            {
+                Padding = 5,
+                Spacing = 5,
+                HorizontalContentAlignment = HorizontalAlignment.Stretch,
+                Items =
+                {
+                    devicePathGroup,
+                    tabletGroup,
+                    reconnectBox,
+                    connectButton
+                }
+            };
         }
+
+        private readonly ComboBox devicePathText;
+        private readonly CheckBox reconnectBox;
+
+        private readonly DropDown<string> tablet;
+
+        private readonly Group devicePathGroup, tabletGroup;
     }
 }
 
